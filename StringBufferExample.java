c78efef23dfcadae3ad1cad7888d7e145fcc3b9a
mayu875 <shivangekarmayuri@gmail.com>
2024-01-18 21:22:18 +0530

    String Buffer Commit

---
+++ StringBufferExample.java
@@ -0,0 +1,33 @@
+public class StringBufferExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringBuffer sb = new StringBuffer("Pooja");
+
+		StringBuffer sb1= new StringBuffer("Mayuri");
+		
+		System.out.println(sb);
+		System.out.println(sb1);
+	}
+
+}
+......................................................................................................................
+public class StringBufferReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer sb = new StringBuffer("CharacterSquence");	
+		System.out.println(sb);
+
+
+		//Reverse string
+		StringBuffer sb1 = new StringBuffer("CharacterSquence");	
+		sb1.reverse();
+		System.out.println(sb1);
+
+	}
+
+}
+
